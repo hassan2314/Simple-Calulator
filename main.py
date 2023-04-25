d83f3f2e0e940752c273af2947b49009a3ea9401
hassan2314 <hassan87691@gmail.com>
2023-04-25 14:53:55 +0500

    fun added

---
+++ main.py
@@ -0,0 +1,12 @@
+from addition import add
+from substraction import sub
+from product import pro
+from pro import product
+
+if __name__=="__main__":
+    A=6
+    B=5
+    print(add(A,B))
+    print(sub(A,B))
+    print(pro(A,B))
+    print(product(A,B))
